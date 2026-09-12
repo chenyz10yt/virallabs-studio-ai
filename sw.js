c2386ca78269ae6e4758152cfb6d8c15f45bbaf6
chenyz10yt <chenyz10yt@gmail.com>
2026-09-11 21:29:18 -0500

    ViralLabs Auto-Deploy: sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'virallabs-studio-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((k) => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
